5765ed8ae70fec78e4edefc8c6f03969fee08035
psjadhav19teen <42905297+psjadhav19teen@users.noreply.github.com>
2023-09-11 15:41:48 +0530

    Add files via upload

---
+++ fetchuser.js
@@ -0,0 +1,11 @@
+var userdata = require("./user.json")
+console.log(userdata)
+
+
+//error- to required server
+// function userfetch() {
+//     fetch("./user.json")
+//         .then(response => response.json())
+//         .then(data => console.log(data))
+// }
+// userfetch()
